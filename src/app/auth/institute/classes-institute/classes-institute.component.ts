224effdfaed4a742cb2267a7405a54419bfd90cf
RochaaP <rochpathiraja@gmail.com>
2020-01-14 21:18:33 +0530

    layout changed

--- src/app/auth/institute/classes-institute/classes-institute.component.ts
+++ src/app/auth/institute/classes-institute/classes-institute.component.ts
@@ -3,6 +3,9 @@ import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { AuthenticationService } from 'src/app/service/auth/authentication.service';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+
 
 @Component({
   selector: 'app-classes-institute',
@@ -12,9 +15,19 @@ import { AngularFirestore } from '@angular/fire/firestore';
 export class ClassesInstituteComponent implements OnInit {
 
 
-  tabs = [];
-  cards = [];
-  tempNew = [];
+  // tabs = [];
+  // cards = [];
+  // tempNew = [];
+
+
+  classListAll = [];
+  classListAllLength: number;
+  classListNewlyAdded = [];
+
+  editListAll: boolean;
+  editListNew: boolean;
+  editValNum: number;
+
 
   selected = new FormControl(0);
   selectedCard = new FormControl(0);
@@ -24,6 +37,7 @@ export class ClassesInstituteComponent implements OnInit {
   nameInput: string;
   id: string;
 
+  primaryInput = '';
   classInput = '';
   timeInput = '';
   dayInput = '';
@@ -43,10 +57,15 @@ export class ClassesInstituteComponent implements OnInit {
   response: any = [];
   recievedContent: any;
 
+  MESSAGE_SUCCESS = 'DETAILS UPDATED';
+  MESSAGE_FAIL = 'UPDATE FAILED';
+
   constructor(
     private http: HttpClient,
     private afs: AngularFirestore,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private snackBar: MatSnackBar,
+    private spinnerService: Ng4LoadingSpinnerService
   ) { }
 
   ngOnInit() {
@@ -59,33 +78,33 @@ export class ClassesInstituteComponent implements OnInit {
     }
   }
 
-  addTab(selectAfterAdding: boolean) {
-    this.tabs.push(this.nameInput);
-    this.nameInput = '';
+  // addTab(selectAfterAdding: boolean) {
+  //   this.tabs.push(this.nameInput);
+  //   this.nameInput = '';
 
-    if (selectAfterAdding) {
-      // this.selected.setValue(this.tabs.length - 1);
-    }
-  }
+  //   if (selectAfterAdding) {
+  //     // this.selected.setValue(this.tabs.length - 1);
+  //   }
+  // }
 
-  removeTab(index: number) {
-    delete this.tabs[index];
-  }
-  removeDe(index: number) {
-    // delete this.cards[index];
-    // this.cards.splice(index, 1);
-    // delete this.cards[index];
-    this.cards.forEach(o => {
-      console.log('asjdkgh' + o.index);
-  });
-    console.log(this.cards);
-  }
+  // removeTab(index: number) {
+  //   delete this.tabs[index];
+  // }
+  // removeDe(index: number) {
+  //   // delete this.cards[index];
+  //   // this.cards.splice(index, 1);
+  //   // delete this.cards[index];
+  //   this.cards.forEach(o => {
+  //     console.log('asjdkgh' + o.index);
+  // });
+  //   console.log(this.cards);
+  // }
 
   submit(index: number) {
     this.details = {
       index,
       subject: this.subjectInput,
-      primary: this.tabs[index],
+      primary: this.primaryInput,
       city: this.cityInput,
       district: this.DistrictInput,
       class: this.classInput,
@@ -95,26 +114,37 @@ export class ClassesInstituteComponent implements OnInit {
       classType:  this.classTypeInput
 
     };
-    this.cards.push(this.details);
-    this.tempNew.push(this.details);
-    (document.getElementById('buttonUploadAll') as HTMLInputElement).disabled = false;
+    this.classListAll.push(this.details);
+    this.classListNewlyAdded.push(this.details);
+    // (document.getElementById('buttonUploadAll') as HTMLInputElement).disabled = false;
 
   }
   upload() {
+    this.spinnerService.show();
+
     let values;
     values = {
       id: this.id,
       registerItem: this.registerItem,
       email: this.userDetails.email,
-      content : this.cards
+      content : this.classListAll
     };
 
     this.postAPIData(values).subscribe((response) => {
       console.log('response from POST API is ', response);
+      this.response = response;
+      if (this.response.status === 200) {
+        this.openSnackBar(this.MESSAGE_SUCCESS);
+        this.spinnerService.hide();
+      }
+      else if (this.response.status === 400) {
+        this.openSnackBar(this.MESSAGE_FAIL);
+        this.spinnerService.hide();
+      }
     }, (error) => {
       console.log('error during post is ', error);
     });
-
+    this.primaryInput = '';
     this.classInput = '';
     this.timeInput = '';
     this.dayInput = '';
@@ -124,10 +154,15 @@ export class ClassesInstituteComponent implements OnInit {
     this.DistrictInput = '';
     this.subjectInput = '';
 
-    (document.getElementById('buttonUploadAll') as HTMLInputElement).disabled = true;
-    this.cards.splice(0, this.cards.length);
-    this.tabs.splice(0, this.tabs.length);
-    this.tempNew.splice(0, this.tempNew.length);
+    // (document.getElementById('buttonUploadAll') as HTMLInputElement).disabled = true;
+    // this.classListAll.splice(0, this.classListAll.length);
+    this.classListNewlyAdded.splice(0, this.classListNewlyAdded.length);
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'Done', {
+      duration: 5000,
+    });
   }
 
   postAPIData(userValues: object) {
@@ -140,13 +175,14 @@ export class ClassesInstituteComponent implements OnInit {
       if (response[0].id ==  null){
         const id = this.afs.createId();
         this.id = id;
+        this.classListAllLength = 0;
+
       }
       else {
         this.id = response[0].id;
+        this.classListAll = response[0].data.content;
+        this.classListAllLength = this.classListAll.length;
       }
-      this.cards = response[0].data.content;
-      console.log("thist is the response card" + this.cards);
-      console.log('response from POST API is ', response);
     }, (error) => {
       console.log('error during post is ', error);
     });
@@ -156,24 +192,123 @@ export class ClassesInstituteComponent implements OnInit {
   getAPIData() {
     return this.http.post('api/getClasses/institute', {email: this.email});
   }
-
-
-  delete(index: number) {
-
+  deleteFromNew(index: number) {
+    this.classListNewlyAdded.splice(index, 1);
+    this.classListAll.splice(index + this.classListAllLength, 1);
   }
-  update(id: string) {
-    // this.updateAPIData().subscribe((response) => {
-    //   // this.response = response[0].data.content;
-    //   this.response = response;
-    //   console.log(this.response);
-    //   console.log('response from POST API is ', response);
-    // }, (error) => {
-    //   console.log('error during post is ', error);
-    // });
+
+  deleteFromAll(index: number) {
+    this.classListAll.splice(index, 1);
+    if (index > this.classListAllLength - 1) {
+      this.classListNewlyAdded.splice(index - this.classListAllLength, 1);
+    }
+  }
+
+  editFromAll(index: number) {
+    this.editListAll = true;
+    this.editValNum = index;
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    this.primaryInput = this.classListAll[index].primary;
+    // this.placeInput = this.classListAll[index].place;
+    this.classInput = this.classListAll[index].class;
+    this.timeInput = this.classListAll[index].time;
+    this.dayInput = this.classListAll[index].day;
+    this.mediumInput = this.classListAll[index].medium;
+    this.classTypeInput = this.classListAll[index].classType;
+    this.cityInput = this.classListAll[index].city;
+    this.DistrictInput = this.classListAll[index].district;
+    this.subjectInput = this.classListAll[index].subject;
+  }
+
+  updateEditedFromAll() {
+    this.editListAll = false;
+    this.classListAll[this.editValNum].primary = this.primaryInput;
+    // this.classListAll[this.editValNum].place = this.placeInput;
+    this.classListAll[this.editValNum].class = this.classInput;
+    this.classListAll[this.editValNum].time = this.timeInput;
+    this.classListAll[this.editValNum].day = this.dayInput;
+    this.classListAll[this.editValNum].medium = this.mediumInput;
+    this.classListAll[this.editValNum].classType = this.classTypeInput;
+    this.classListAll[this.editValNum].city = this.cityInput;
+    this.classListAll[this.editValNum].district = this.DistrictInput;
+    this.classListAll[this.editValNum].subject = this.subjectInput;
+
+    if (this.editValNum > this.classListAllLength - 1) {
+
+      const val = this.editValNum - this.classListAllLength;
+      this.classListNewlyAdded[val].primary = this.primaryInput;
+      // this.classListNewlyAdded[val].place = this.placeInput;
+      this.classListNewlyAdded[val].class = this.classInput;
+      this.classListNewlyAdded[val].time = this.timeInput;
+      this.classListNewlyAdded[val].day = this.dayInput;
+      this.classListNewlyAdded[val].medium = this.mediumInput;
+      this.classListNewlyAdded[val].classType = this.classTypeInput;
+      this.classListNewlyAdded[val].city = this.cityInput;
+      this.classListNewlyAdded[val].district = this.DistrictInput;
+      this.classListNewlyAdded[val].subject = this.subjectInput;
+    }
+  }
+
+  editFromNew(index: number) {
+    this.editListNew = true;
+    this.editValNum = index;
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    this.primaryInput = this.classListNewlyAdded[index].primary;
+    // this.placeInput = this.classListNewlyAdded[index].place;
+    this.classInput = this.classListNewlyAdded[index].class;
+    this.timeInput = this.classListNewlyAdded[index].time;
+    this.dayInput = this.classListNewlyAdded[index].day;
+    this.mediumInput = this.classListNewlyAdded[index].medium;
+    this.classTypeInput = this.classListNewlyAdded[index].classType;
+    this.cityInput = this.classListNewlyAdded[index].city;
+    this.DistrictInput = this.classListNewlyAdded[index].district;
+    this.subjectInput = this.classListNewlyAdded[index].subject;
+  }
+
+
+  updateEditedFromNew() {
+    this.editListNew = false;
+    const val = this.editValNum + this.classListAllLength;
+
+    this.classListAll[val].primary = this.primaryInput;
+    // this.classListAll[val].place = this.placeInput;
+    this.classListAll[val].class = this.classInput;
+    this.classListAll[val].time = this.timeInput;
+    this.classListAll[val].day = this.dayInput;
+    this.classListAll[val].medium = this.mediumInput;
+    this.classListAll[val].classType = this.classTypeInput;
+    this.classListAll[val].city = this.cityInput;
+    this.classListAll[val].district = this.DistrictInput;
+    this.classListAll[val].subject = this.subjectInput;
+
+
+
+    this.classListNewlyAdded[this.editValNum].primary = this.primaryInput;
+    // this.classListNewlyAdded[this.editValNum].place = this.placeInput;
+    this.classListNewlyAdded[this.editValNum].class = this.classInput;
+    this.classListNewlyAdded[this.editValNum].time = this.timeInput;
+    this.classListNewlyAdded[this.editValNum].day = this.dayInput;
+    this.classListNewlyAdded[this.editValNum].medium = this.mediumInput;
+    this.classListNewlyAdded[this.editValNum].classType = this.classTypeInput;
+    this.classListNewlyAdded[this.editValNum].city = this.cityInput;
+    this.classListNewlyAdded[this.editValNum].district = this.DistrictInput;
+    this.classListNewlyAdded[this.editValNum].subject = this.subjectInput;
   }
 
 
   updateAPIData() {
     return this.http.post('api/getClasses/person', {email: this.email});
+  }
+
+  clear() {
+    this.primaryInput = '';
+    this.classInput = '';
+    this.timeInput = '';
+    this.dayInput = '';
+    this.mediumInput = '';
+    this.classTypeInput = '';
+    this.cityInput = '';
+    this.DistrictInput = '';
+    this.subjectInput = '';
   }
 }
--- src/app/auth/institute/profileview/institute-profile/institute-profile.component.ts
+++ src/app/auth/institute/profileview/institute-profile/institute-profile.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-institute-profile',
@@ -13,6 +17,13 @@ export class InstituteProfileComponent implements OnInit {
 
   // @Input() childMessageEmail: string;
 
+
+  fileProfile: AngularFireUploadTask;
+  percentageProfile: Observable<number>;
+
+  fileBackground: AngularFireUploadTask;
+  percentageBackground: Observable<number>;
+
   id: string;
   nameInput = '';
   emailInput = '';
@@ -20,19 +31,34 @@ export class InstituteProfileComponent implements OnInit {
   street1Input = '';
   street2Input = '';
   cityInput = '';
-  townInput = '';
+  districtInput = '';
   provinceInput = '';
   downloadURL = '';
   backgroundImageURL = '';
 
+  uploadProfile: boolean;
+  uploadProfilePath = '';
+
+  uploadBackground: boolean;
+  uploadBackgroundPath = '';
+
+  response: any;
+
+  MESSAGE_SUCCESS = 'DETAILS UPDATED';
+  MESSAGE_FAIL = 'UPDATE FAILED';
 
   constructor(
     private afs: AngularFirestore,
     private afStorage: AngularFireStorage,
-    private http: HttpClient
+    private http: HttpClient,
+    private spinnerService: Ng4LoadingSpinnerService,
+    private snackBar: MatSnackBar,
+
     ) { }
 
   ngOnInit() {
+    // this.uploadProfile = false;
+    // this.uploadBackground = false;
     const itemTemp  = JSON.parse( localStorage.getItem('user'));
     if (itemTemp != null) {
       this.emailInput = itemTemp.email;
@@ -46,11 +72,23 @@ export class InstituteProfileComponent implements OnInit {
       this.emailInput = response[0].data.email;
       this.contactInput = response[0].data.contact;
       this.downloadURL =  response[0].data.profileImagePath;
+      if (!this.downloadURL) {
+        this.uploadProfile = false;
+      }
+      else {
+        this.uploadProfile = true;
+      }
       this.backgroundImageURL = response[0].data.backgroundImagePath;
+      if (!this.backgroundImageURL) {
+        this.uploadBackground = false;
+      }
+      else {
+        this.uploadBackground = true;
+      }
       this.street1Input = response[0].data.streetNo1;
       this.street2Input = response[0].data.streetNo2;
       this.cityInput = response[0].data.city;
-      this.townInput = response[0].data.town;
+      this.districtInput = response[0].data.district;
       this.provinceInput = response[0].data.province;
     }, ( error) => {
       console.log('error is ', error);
@@ -62,7 +100,7 @@ export class InstituteProfileComponent implements OnInit {
   }
 
   updateValues() {
-
+    this.spinnerService.show();
     let userValues = {};
 
     userValues = {
@@ -73,15 +111,27 @@ export class InstituteProfileComponent implements OnInit {
         streetNo1: this.street1Input,
         streetNo2: this.street2Input,
         city: this.cityInput,
-        town: this.townInput,
+        district: this.districtInput,
         province: this.provinceInput,
         profileImagePath: this.downloadURL,
         backgroundImagePath: this.backgroundImageURL
       };
     this.postAPIData(userValues).subscribe((response) => {
         console.log('response from POST API is ', response);
+        this.response = response;
+        console.log(this.response.status);
+        if (this.response.status === 200) {
+          this.openSnackBar(this.MESSAGE_SUCCESS);
+          this.spinnerService.hide();
+        }
+        else if (this.response.status === 400) {
+          this.openSnackBar(this.MESSAGE_FAIL);
+          this.spinnerService.hide();
+        }
       }, (error) => {
         console.log('error during post is ', error);
+        this.openSnackBar(this.MESSAGE_FAIL);
+        this.spinnerService.hide();
       });
   }
 
@@ -90,10 +140,15 @@ export class InstituteProfileComponent implements OnInit {
   }
 
   upload(event) {
+    this.uploadProfile = true;
+
     const randomId = Math.random().toString(36).substring(2);
     const path = `profilePictures/${Date.now()}_${randomId}`;
+    this.uploadProfilePath = path;
     // Reference to storage bucket
     const ref = this.afStorage.ref(path);
+    this.fileProfile = this.afStorage.upload(path, event.target.files[0]);
+    this.percentageProfile = this.fileProfile.percentageChanges();
     // The main task
     // this.task = this.afStorage.upload(path, event.target.files[0]);
     // console.log(this.task.downloadURL());
@@ -107,11 +162,31 @@ export class InstituteProfileComponent implements OnInit {
 
     });
   }
+
+  deleteProfile() {
+    if (this.uploadProfilePath) {
+      this.afStorage.ref(this.uploadProfilePath).delete().subscribe(() => {
+      }, (error) => {
+        console.log(error);
+      }, () => {
+        console.log('successfully deleted');
+      });
+    }
+    this.uploadProfile = false;
+    this.downloadURL = '';
+  }
+
   backgroundUpload(event) {
+    this.uploadBackground = true;
+
     const randomId = Math.random().toString(36).substring(2);
     const path = `backgroundImages/${Date.now()}_${randomId}`;
+    this.uploadBackgroundPath = path;
     // Reference to storage bucket
     const ref = this.afStorage.ref(path);
+
+    this.fileBackground = this.afStorage.upload(path, event.target.files[0]);
+    this.percentageBackground = this.fileBackground.percentageChanges();
     // The main task
     // this.task = this.afStorage.upload(path, event.target.files[0]);
     // console.log(this.task.downloadURL());
@@ -123,6 +198,27 @@ export class InstituteProfileComponent implements OnInit {
       this.backgroundImageURL = url; // with this you can use it in the html
       });
 
+    });
+  }
+
+  deleteBackground() {
+    if (this.uploadBackgroundPath) {
+      this.afStorage.ref(this.uploadBackgroundPath).delete().subscribe(() => {
+
+      }, () => {
+        console.log("deleted Failed");
+      }, () => {
+        console.log("successfully deleted");
+      });
+    }
+
+    this.uploadBackground = false;
+    this.backgroundImageURL = '';
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'Done', {
+      duration: 5000,
     });
   }
 }
