263bc4999dd6bf9314335e07d1a0e441af74b83f
eranthawelikala <eranthawelikala@gmail.com>
2020-05-05 23:05:27 +0530

    Updated about.html

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,17 +8,6 @@ export const environment = {
     zoomClientId: "eezr713VT0u99HLZYP6wng",
     redirect_url: "https%3A%2F%2Fmtute.herokuapp.com%2Fzoom"
   },
-  // firebase : {
-  //   apiKey: "AIzaSyAVyXc1Tqi46-xY1RJ_UjyUBujyIP6n5R0",
-  //   authDomain: "questionproject-58e1c.firebaseapp.com",
-  //   databaseURL: "https://questionproject-58e1c.firebaseio.com",
-  //   projectId: "questionproject-58e1c",
-  //   storageBucket: "questionproject-58e1c.appspot.com",
-  //   messagingSenderId: "259814585439",
-  //   appId: "1:259814585439:web:6cd749ecc264126d6817ea",
-  //   measurementId: "G-ETC2N83QMY"
-  // }
-
   firebase : {
     apiKey: "AIzaSyD04gUUUibi9QCt47geqWxi9AX7k28zRHk",
     authDomain: "mtute-sl.firebaseapp.com",
