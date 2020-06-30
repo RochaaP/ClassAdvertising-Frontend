6193fa93035ed66b8e170bfc0fda6a5fdceb42f3
Erantha Yasith Welikala <33684206+eranthaWELIKALA@users.noreply.github.com>
2020-07-01 02:55:18 +0530

    Commented firebase details

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -19,16 +19,16 @@ export const environment = {
   //   measurementId: "G-ETC2N83QMY"
   // }
 
-  firebase : {
-    apiKey: "AIzaSyD04gUUUibi9QCt47geqWxi9AX7k28zRHk",
-    authDomain: "mtute-sl.firebaseapp.com",
-    databaseURL: "https://mtute-sl.firebaseio.com",
-    projectId: "mtute-sl",
-    storageBucket: "mtute-sl.appspot.com",
-    messagingSenderId: "922851090683",
-    appId: "1:922851090683:web:3ce6ee9c254910e310905e",
-    measurementId: "G-VCYP7E3WM3"
-  }
+//   firebase : {
+//     apiKey: "AIzaSyD04gUUUibi9QCt47geqWxi9AX7k28zRHk",
+//     authDomain: "mtute-sl.firebaseapp.com",
+//     databaseURL: "https://mtute-sl.firebaseio.com",
+//     projectId: "mtute-sl",
+//     storageBucket: "mtute-sl.appspot.com",
+//     messagingSenderId: "922851090683",
+//     appId: "1:922851090683:web:3ce6ee9c254910e310905e",
+//     measurementId: "G-VCYP7E3WM3"
+//   }
 };
 
 
