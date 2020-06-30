79b7388ed1fb31ad4338d0399eb1cade87cd621d
Erantha Yasith Welikala <33684206+eranthaWELIKALA@users.noreply.github.com>
2020-07-01 02:55:58 +0530

    Commented firebase details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,14 +4,14 @@ export const environment = {
     zoomClientId: "hPDxBy3PR7eOfPslM0gNnw",//"eezr713VT0u99HLZYP6wng"
     redirect_url: "https%3A%2F%2Fwww.mtute.lk%2Fzoom"
   },
-  firebase : {
-    apiKey: "AIzaSyD04gUUUibi9QCt47geqWxi9AX7k28zRHk",
-    authDomain: "mtute-sl.firebaseapp.com",
-    databaseURL: "https://mtute-sl.firebaseio.com",
-    projectId: "mtute-sl",
-    storageBucket: "mtute-sl.appspot.com",
-    messagingSenderId: "922851090683",
-    appId: "1:922851090683:web:3ce6ee9c254910e310905e",
-    measurementId: "G-VCYP7E3WM3"
-  }
+//   firebase : {
+//     apiKey: "AIzaSyD04gUUUibi9QCt47geqWxi9AX7k28zRHk",
+//     authDomain: "mtute-sl.firebaseapp.com",
+//     databaseURL: "https://mtute-sl.firebaseio.com",
+//     projectId: "mtute-sl",
+//     storageBucket: "mtute-sl.appspot.com",
+//     messagingSenderId: "922851090683",
+//     appId: "1:922851090683:web:3ce6ee9c254910e310905e",
+//     measurementId: "G-VCYP7E3WM3"
+//   }
 };
