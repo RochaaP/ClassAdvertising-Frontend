f92260dc550fadec0ca46f163d6b286a809ee76c
Rochana Pathiraja <rochpathiraja@gmail.com>
2020-07-04 19:56:23 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,25 +9,11 @@ export const environment = {
     redirect_url: "https%3A%2F%2Fwww.mtute.lk%2Fzoom"
   },
   // firebase : {
-  //   apiKey: "AIzaSyAVyXc1Tqi46-xY1RJ_UjyUBujyIP6n5R0",
-  //   authDomain: "questionproject-58e1c.firebaseapp.com",
-  //   databaseURL: "https://questionproject-58e1c.firebaseio.com",
-  //   projectId: "questionproject-58e1c",
-  //   storageBucket: "questionproject-58e1c.appspot.com",
-  //   messagingSenderId: "259814585439",
-  //   appId: "1:259814585439:web:6cd749ecc264126d6817ea",
-  //   measurementId: "G-ETC2N83QMY"
+  
   // }
 
 //   firebase : {
-//     apiKey: "AIzaSyD04gUUUibi9QCt47geqWxi9AX7k28zRHk",
-//     authDomain: "mtute-sl.firebaseapp.com",
-//     databaseURL: "https://mtute-sl.firebaseio.com",
-//     projectId: "mtute-sl",
-//     storageBucket: "mtute-sl.appspot.com",
-//     messagingSenderId: "922851090683",
-//     appId: "1:922851090683:web:3ce6ee9c254910e310905e",
-//     measurementId: "G-VCYP7E3WM3"
+
 //   }
 };
 
@@ -44,14 +30,4 @@ export const environment = {
 // serviceaccountkey.js in backend
 //
 // {
-//   "type": "service_account",
-//   "project_id": "questionproject-58e1c",
-//   "private_key_id": "d19574a7a70ec9d1ecaddf0921ad9f7f9d365525",
-//   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCqxBsEMboQgkNT\na8ZQ53KnQ3fxY5gUDujcJ0hXygL0TvBK86LPg1GjNcdFXfT6xbL27s4rzoeA89Tk\nS97hnnZbhxd45uhcUUbm3ePyOQzjKpdG0N47ou1p4Y6KEBd9bgizAEdPH9mu9LMM\nw2KmUc7a20MAkLWZCghCAiR38zkNPQPh/OKfIHHJPDS5fcVr+X3kiCyeHl3nUuZZ\n3XATA9oxZzyOuCK4lfruZ3JmYYtS0eWGoB09jJIyi+41qtQxq1BWzgLaA/fiJC8r\n7AYQ3WVzmxE7tuv62X+EQfVRUcF1jBkb5Fqwlm7dGDPSJ6O7PqJ9HqZt/dGemqiB\ndSooCLmDAgMBAAECggEAO8oBs4w9mz4gmHqpqvX+XLXwruGyA4Pc4AgY+jPOqP9i\nfrujC4js8noH1S+6cHr72B1wrkHXi7zTk0NFPDWhNxarjzc5r+j9LCQQhWSYMIlf\nAfprdPHssh+5KnnL36zA+vtj/D4ol2sxrYxTY+xMwq/2W6JPEtBCZ+ocHqtYFFvw\nHUudxowS7zzr/eVO447oRWNyPhSS0npDQn7FMKNeXWydBIybNqH+v2ZWVQ5zwWc3\n+kucmwL8aCv4Upca/UpXu+n/ElLsQeXL6o0UbQCGDT9Vl/LzWvuZD/mW5f+8e62D\n0A94bFN1rK0aHmEjPAKFqPpPei2/KNi0gGMsVgT7EQKBgQDX4151q5bODD4grZDL\n7HmSOTJOxQnzniq73ejrH6Nja/OSze5R9DULFrFGMXrZblrojIGKNDIqT4ot1U5K\ncemMVXk4qXZT363NAGmkNM7CXq08A9OLT1g2z503oE3CxjkkVuMFvQ6vmlb2QaMa\neJs4zS6vUDMUwvZRH9QbOXwYfQKBgQDKfoV9Xz7UO44NUjhieOWxeRbQPf5WWSps\nKn02v9OzYzfKrKp9hltMbVULytCvWbASiXyXpf8sYv3RnkmCcr0uFF4PpbKsl1BG\nRkYrkg5KY5zIIOCNNTvvE/sVYxzp1BioQxr8l9L3EJBMdAgwTo5rdKEJyYBJKDj6\nKqzancK5/wKBgQDS0zdrsiWTEmGZh+XfPw95vGFkiwcs4UjaLdtHwZ3NeZ5jvrb9\nUX57Va3I3okZzKr6bjwSuuZCa2qFV8kc6iEo6k93BgaH4efYSo7fzLIdHlgOQ65S\nXSnIXPcrW2CU1P74feE1uWqeEhraYT7mmuD6l2T/z1IS9FGW5347F+gYAQKBgEo4\nJ4t1Xp0BmyZszXfiYiYzgNYg8tkJY5Az3qMzmk+0/R0PPoo1tb8Ej9h7pROwZuXU\neNNIp4fIXMn5H25tycNa+cL4c83oxCaN2Whc51oCutNroYKAUZBmnXAQXOejssnx\nlrGgoFWoP4uWN+4kzPS70c65iYjG8mNXQvX6PwZXAoGADSTzYoBGLEbVSZqNT+Bz\nzRI+oqs5TTLd8Z85Yj8SN7LMP9PMaYk9kYX8k35fUkYv/C5aV5TWE3S90INepBx0\n9zPmq4CUboMd0WUwVRy88LSdWYndWDb0VadlCaI6899tMfyHwlgNHzHCXY1PrqlV\nCoR5+Lg+4oCK3EqfyNRi49E=\n-----END PRIVATE KEY-----\n",
-//   "client_email": "firebase-adminsdk-kwpx0@questionproject-58e1c.iam.gserviceaccount.com",
-//   "client_id": "113846447050062899523",
-//   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-//   "token_uri": "https://oauth2.googleapis.com/token",
-//   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-//   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-kwpx0%40questionproject-58e1c.iam.gserviceaccount.com"
-// }
+//   // }
